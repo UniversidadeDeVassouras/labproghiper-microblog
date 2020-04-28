36b1e36cd1ad944f79daf6e0d541fef2549ff67a
Tássio Auad <auadtassio@gmail.com>
2020-04-27 22:20:30 -0300

    feat: Controlador para Login
    Criado controlado para lidar com requisições de visualização do formulario login e requisições para realizar autenticação/login

--- application/controller/hello_controller.py
+++ application/controller/hello_controller.py
@@ -1,6 +1,26 @@
 from application import app
-from flask import render_template
+from flask import render_template, request
+from application.model.dao.usuario_dao import UsuarioDAO
+from application.model.entity.usuario import Usuario
 
-@app.route("/")
+usuarioAutenticado = None
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template("index.html")
+    if request.method == 'POST':
+       email = request.form['email']
+       senha = request.form['senha']
+       usuario = Usuario(email, senha, None)
+       autenticacao = UsuarioDAO().login(usuario)
+       if autenticacao.is_sucesso():
+         usuarioAutenticado = autenticacao.get_usuario_autenticado()
+         return render_template("index.html")
+       elif autenticacao.is_nao_existe():
+         return render_template("register.html", usuario = usuario)
+       elif autenticacao.is_invalido():
+         return render_template("index.html", login_invalido = True, usuario = usuario)
+
+    if request.method == 'GET':   
+          return render_template("index.html")
+                 
+   
